45144ef543ecb94501f4a9c1c657e00b3ff7e65f
jmmark <jmmarkman@gmail.com>
2017-06-27 22:41:58 -0700

    Added custom geom, size still a problem

---
+++ R/geom_script.R
@@ -0,0 +1,61 @@
+# file for creating the quake geoms:
+# 1 geom: timeline plot of earthquakes showing dots for each one within xmin and xmax days
+# 2 geom: annotations above timeline labeling the earthquakes
+# use stat to subset the data or the x largest earthquakes as required
+
+library(ggplot2)
+library(grid)
+
+geom_timeline <- function(mapping = NULL, data = NULL, stat = 'timeline',
+                          position = 'identity', na.rm = FALSE, show.legend = NA,
+                          inherit.aes = TRUE, x_min, x_max, ...) {
+    ggplot2::layer(
+        geom = GeomTimeline, mapping = mapping, data = data, stat = stat,
+        position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+        params = list(na.rm = na.rm, x_min = x_min, x_max = x_max, ...)
+    )
+}
+
+GeomTimeline <- ggplot2::ggproto('GeomTimeline', Geom,
+        required_aes = c('x'),
+        default_aes = aes(y = NULL, color = 'black',
+                          fill = 'black', size = .5, alpha = 0.5),
+        draw_key = draw_key_point,
+        draw_group = function(data, panel_params, coord) {
+            coords <- coord$transform(data, panel_params)
+            print(coords)
+            grid::pointsGrob(
+                coords$x,
+                coords$y,
+                pch = 21,
+                size = unit(coords$size, 'char'),
+                gp = grid::gpar(
+                    col = coords$colour,
+                    fill = coords$fill,
+                    alpha = coords$alpha
+                )
+            )
+        }
+)
+
+stat_timeline <- function(mapping = NULL, data = NULL, geom = 'timeline',
+                          position = 'identity', na.rm = FALSE, show.legend = NA,
+                          inherit.aes = TRUE, x_min, x_max, ...) {
+    ggplot2::layer(
+        stat = StatTimeline, mapping = mapping, data = data, stat = stat,
+        position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+        params = list(x_min = x_min, x_max = x_max, na.rm = na.rm, ...)
+    )
+}
+
+# all this stat does is filter the data so that x is between x_min and x_max
+StatTimeline <- ggplot2::ggproto('StatTimeline',Stat,
+        required_aes = c('x'),
+        compute_group = function(data, scales, params, x_min, x_max) {
+            if(!('y' %in% names(data))) {
+                data$y <- 1
+            }
+            print(names(data))
+            return(data[data$x >= x_min & data$x <= x_max,])
+        }
+)
